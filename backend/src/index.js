c476cedb17326499a8c6ba2a17dddc554b371d82
Singh-Arekh <singharekh@gmail.com>
2025-03-07 14:46:24 +0530

    cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,22 +1,32 @@
 import express, { json } from 'express';
 import dbConnect from './config/db.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import cors from 'cors'
-// const dotenv = require('dotenv').config();
+import cors from 'cors';
+
 dotenv.config();
 dbConnect();
 const app = express();
-app.use(cors())
-//middleware
-app.use(json());
-//Routes
-app.use('/api/auth',authRoutes)
-app.use('/api/user',userRoutes)
 
-//server
+// Configure CORS to allow specific origins
+const corsOptions = {
+  origin: 'https://rbac-express-gsei-git-main-singh-arekhs-projects.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  credentials: true,
+};
+
+app.use(cors(corsOptions)); 
+
+// Middleware
+app.use(json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/user', userRoutes);
+
+// Server
 const PORT = process.env.PORT || 8000;
-app.listen(PORT,()=>{
-    console.log((`server is running on ${PORT}`))
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
